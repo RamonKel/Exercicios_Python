71f5ce2f59586768085b6ac43c52a171db650cfa
Ramon Kelvin <ramonkelvin890@gmail.com>
2024-08-29 16:45:09 -0300

    Exercicios

---
+++ Python_Exercicios/Desconto.py
@@ -0,0 +1,5 @@
+Produto = float(input('Digite o valor do produto: R$ '))
+Desconto = Produto*0.95
+print('O valor do produto é de R$ {:.2f}, com desconto de 5% seu valor é R$ {:.2f}'.format(Produto,Desconto))
+
+# Verifica o valor do produto e aplica o desconto de 5%
---
+++ Python_Exercicios/Reajuste_Salarial.py
@@ -0,0 +1,5 @@
+Salário = float(input('Digite o salário bruto do funcionário: R$ '))
+Reajuste = Salário*1.15
+print('O salário atual do funcionário é R$ {:.2f}, após o acréscimo de 15% o salário é de R$ {:.2f}'.format(Salário,Reajuste))
+
+# Verifica o salário e aplica 15% a mais no salário inicial
