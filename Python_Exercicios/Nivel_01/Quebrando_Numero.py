3d2cc2a4b9141a138e76c69596e5b47d19711e4b
Ramon Kelvin <ramonkelvin890@gmail.com>
2024-08-30 16:40:11 -0300

    Exercicios2 - 30/08

--- Python_Exercicios/Nivel_01/Quebrando_Numero.py
+++ Python_Exercicios/Nivel_01/Quebrando_Numero.py
@@ -1,5 +1,4 @@
-import math
-from math import floor, trunc
+from math import trunc
 
 N = float(input('Digite um número real(Ex: 1.25): '))
 print('A porção inteira do número {}, é de {}'.format(N,trunc(N)))
