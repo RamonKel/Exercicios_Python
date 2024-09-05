1c93a479315e99aff2f596ee2d8324b2deb59f3b
Ramon Kelvin <ramonkelvin890@gmail.com>
2024-09-05 16:50:12 -0300

    Exercicios 03 - 04/09

--- Python_Exercicios/Nivel_01/Mp3.py
+++ Python_Exercicios/Nivel_01/Mp3.py
@@ -1,7 +1,9 @@
 import pygame
-pygame.init()
-pygame.mixer.music.load('Music/giornos-theme-but-only-the-best-part-is-in_vwd15lya_lyb0-online-audio-converter.mp3')
-pygame.mixer.music.play()
+pygame.init() #Inicia a biblioteca
+pygame.mixer.music.load('Music/giornos-theme-but-only-the-best-part-is-in_vwd15lya_lyb0-online-audio-converter.mp3') #Carrega a musica
+pygame.mixer.music.play() #
 pygame.event.wait()
 input()
 pygame.event.wait()
+
+#Realiza a reprodução de um audio em MP3 utilizando a biblioteca PyGame
